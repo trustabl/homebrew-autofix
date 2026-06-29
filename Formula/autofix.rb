4b01cec11e09ec98fa9895525d5c3b104c38c3ea
moisesdelacruz-tr <moises.delacruz@trustabl.ai>
2026-06-29 22:29:10 +0800

    release(formula): bump to v0.2.0

--- Formula/autofix.rb
+++ Formula/autofix.rb
@@ -1,24 +1,24 @@
 class Autofix < Formula
   desc "Behavioral attestation and AI-powered fixes for agent tools"
   homepage "https://github.com/trustabl/autofix"
-  version "0.1.0"
+  version "0.2.0"
   license :cannot_represent
 
   on_macos do
     on_arm do
-      url "https://github.com/trustabl/autofix/releases/download/v0.1.0/autofix_0.1.0_darwin_arm64.tar.gz"
-      sha256 "77133817cef20d4353caa61d2af7c6b0287c3d55b918c70cdf275c2f8ac5f7fd"
+      url "https://github.com/trustabl/autofix/releases/download/v0.2.0/autofix_0.2.0_darwin_arm64.tar.gz"
+      sha256 "3f5b7293f999d463b568f2562b06c40008a849c24e050132bdef7935837092ce"
     end
     on_intel do
-      url "https://github.com/trustabl/autofix/releases/download/v0.1.0/autofix_0.1.0_darwin_amd64.tar.gz"
-      sha256 "ce461c6c1af4e7bbbb810947e483afc4c86f722288542d8ce0bb267d098f380d"
+      url "https://github.com/trustabl/autofix/releases/download/v0.2.0/autofix_0.2.0_darwin_amd64.tar.gz"
+      sha256 "a0d3275561b490746065c1d213bc92dd61e408f7001800e8a9b90ed4eda131ca"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/trustabl/autofix/releases/download/v0.1.0/autofix_0.1.0_linux_amd64.tar.gz"
-      sha256 "98805e9d4e4044db9f4d7b33be10c67dc28282454633836830fe3c7ae3d0267e"
+      url "https://github.com/trustabl/autofix/releases/download/v0.2.0/autofix_0.2.0_linux_amd64.tar.gz"
+      sha256 "43cfd6de9236b924a20ecc048c6c2e736ff364059fe9b4b907d61fc766935459"
     end
   end
 
